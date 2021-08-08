5f3b5db7887853765716a1075ccbafd2ab1b1a8c
Meg Viar <meg.viar@panoply.fm>
2021-08-08 20:19:37 +0000

    Create insert statements for issue seed data and write to seed file

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
 import requests
 import json
 import re
+from gcd_issues import gcd_issue_ids
 
 # DB Schema: https://miro.com/app/board/o9J_l4VnWWE=/
 # Combine data from multiple sources to create BMR database records representing issues
 
-# bmr_issue_base = dict(
-#   id=None,#(int)
-#   gcd_id=None,#(int)
-#   gb_id=None,#(int)
-#   gcd_series_id=None,#(int)
-#   title=None,#(str)
-#   subtitle=None,#(str)
-#   description=None,#(str)
-#   snippet=None,#(str)
-#   publisher=None,#(str)
-#   published_date=None,#(datetime)
-#   maturity_rating=None,#(string)
-#   language=None,#(tinytext)
-#   isbn=None,#(int)
-#   page_count=None,#(int)
-#   thumbnail_url=None,#(text)
-#   cover_url=None#(text)
-# )
-
-# Make another request for issue by GB id to get categories?
-
-# For ISBNS, split on semi-colon and get first item (some have multiple) and remove all non-numerical characters
-
-  #  <div class="coverImage">
-  #           <a href="/upload_cover/1667004/"><img class="no_cover" src="https://files1.comics.org/static/img/nocover_medium.png" alt="No image yet"class="cover_img"></a>
-
-
 def get_gcd_cover(gcd_id):
-  print(f'getting cover from : https://www.comics.org/issue/{gcd_id}')
   req = requests.get(f'https://www.comics.org/issue/{gcd_id}').text
-  cover_url = re.search("https://files1\.comics\.org//img/gcd/covers_by_id/.*\.jpg", req, re.M)
-  print(cover_url.group() if cover_url else None)
-  # change width to 400px
-  return cover_url
+  cover_regex = "https://files1\.comics\.org//img/gcd/covers_by_id/.*\.jpg"
+  cover_url = re.search(cover_regex, req, re.M)
+  # Change width to 400px here or on front-end?
+  # cover_url = re.sub("\/w\d{3}\/", "/w400/", cover_url)
+  return cover_url.groups() if cover_url else None
 
 def format_bmr_issue(gb_issue, gcd_issue):
   try:
@@ -48,16 +22,16 @@ def format_bmr_issue(gb_issue, gcd_issue):
       gcd_series_id = gcd_issue['series_id'],
       gb_id = gb_issue['id'],
       title=gb_issue['volumeInfo']['title'],
-      subtitle=gb_issue['volumeInfo'].get('subtitle', None),
-      description=gb_issue['volumeInfo'].get('description', None),
-      publisher=gb_issue['volumeInfo'].get('publisher', None),
-      published_date=gb_issue['volumeInfo']['publishedDate'],
+      subtitle=gb_issue['volumeInfo'].get('subtitle'),
+      description=gb_issue['volumeInfo'].get('description'),
+      publisher=gb_issue['volumeInfo'].get('publisher'),
+      published_date=gb_issue['volumeInfo'].get('publishedDate'),
       mature_content= not (gb_issue['volumeInfo']['maturityRating'] == "NOT_MATURE"),
-      language=gb_issue['volumeInfo']['language'],
-      page_count=gb_issue['volumeInfo']['pageCount'],
-      thumbnail_url=gb_issue['volumeInfo'].get('imageLinks', {}).get('thumbnail', None),
+      language=gb_issue['volumeInfo'].get('language'),
+      page_count=gb_issue['volumeInfo'].get('pageCount'),
+      thumbnail_url=gb_issue['volumeInfo'].get('imageLinks', {}).get('thumbnail'),
       reader_link=gb_issue['accessInfo']['webReaderLink'] if gb_issue['accessInfo']['accessViewStatus'] != "NONE" else None,
-      snippet=gb_issue.get('searchInfo', {}).get('textSnippet', None),
+      snippet=gb_issue.get('searchInfo', {}).get('textSnippet'),
       cover_url=None,
       # Represented as relationships in database
       genres = gb_issue['volumeInfo'].get('categories', [])
@@ -69,22 +43,43 @@ def format_bmr_issue(gb_issue, gcd_issue):
   issue['cover_url'] = get_gcd_cover(issue['gcd_id'])
   return issue
 
+FETCH_LIMIT = 2
 def get_issues():
-  gcd_issues = [
-    dict(isbn='0930193466', id=1008760, series_id=567)
-    # dict(isbn='1302914790', id=1667004, series_id=789)
-  ]
   bmr_issues = []
-  for issue in gcd_issues: 
-    req = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}".format(isbn=issue['isbn']))
-    if not req.json()['items']:
+  for issue in gcd_issue_ids[:FETCH_LIMIT]: 
+    # Some issues have multiple ISBNs with semi-colons as a delimiter
+    issue['isbn'] = re.search("[\d,-]*[^;]", issue['isbn']).group().replace('-','');
+    isbn = issue['isbn']
+    print(f'Fetching Google Books records for ISBN {isbn}...')
+    req = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}')
+    if not req.json().get('items', None):
+      print(f'No Google Books records for ISBN {isbn}.')
       continue
     # Sometimes there are duplicate records for the same ISBN. For now, just grab the first.
     gb_issue = req.json()['items'][0]
     bmr_issue = format_bmr_issue(gb_issue=gb_issue, gcd_issue=issue)
-    if bmr_issue: bmr_issues.append(bmr_issue)
+    if bmr_issue: 
+      bmr_issues.append(bmr_issue)
   return bmr_issues
 
-# unpac dict to make insert statement
+ISSUE_FIELDS = ['gcd_id', 'gb_id', 'gcd_series_id', 'title', 'subtitle', 'description', 'snippet', 'publisher', 'published_date', 'maturity_rating', 'language', 'isbn', 'page_count', 'thumbnail_url', 'cover_url']
+def generate_issue_seed_data(issues):
+  f = open("issue_seeds.py", "a")
+  sql_start = 'INSERT into bmr_isses ('
+  for field in ISSUE_FIELDS:
+    sql_start += f'{field},'
+  sql_start = sql_start[:-1] + ") VALUES ("
+  f.write(sql_start)
+  f.close()
+  for issue in issues:
+      f = open("issue_seeds.py", "a")
+      values = [issue.get(f) for f in ISSUE_FIELDS]
+      # Add quotes to string frields
+      values = [f'"{v}"' if isinstance(v, str) else str(v) for v in values]
+      field_text = ", ".join(values)
+      f.write(json.dumps(f'({field_text})'))
+      f.close()
+  # TO DO - handle storing genres and creators during this loop or in subsequent loops
+issues = get_issues()
 
-get_issues()
+generate_issue_seed_data(issues)
